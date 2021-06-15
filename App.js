8e70c4554580e0a42271b7a680462b24b6759c07
stela <stela.capsa@gmail.com>
2021-06-15 14:28:31 -0500

    react map with hooks

--- App.js
+++ App.js
@@ -1,48 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {MapView} from 'react-native-maps';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default class App extends React.Component{
-
-state = {
-  region:{
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.922,
-    longitudeDelta: 0.0421,
-  }
-}
-
-
-
-
-
-
-  render(){
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hiii</Text>
-      <MapView 
-      initialRegion={this.state.region}
-      showsCompass={true}
-      rotateEnabled={true}
-      style={{flex: 1}}/>
-      
-
-      
-      
-      
-      <StatusBar style="auto" />
+      <MapView style={styles.map} />
     </View>
   );
 }
-}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
