0eb025f3a6c9da302546b1198e9aff55523d1b4e
stela <stela.capsa@gmail.com>
2021-06-15 11:21:58 -0500

    installing map

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapView } from 'expo';
+// import { BrowserRouter } from 'react-router-dom';
+// require('jquery');
 
-export default function App() {
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.922,
+        longitudedelta: 0.421
+      }
+    }
+  }
+
+
+render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>hiii</Text>
+      <MapView
+      initialRegion={this.state.region}
+      showsCompass={true}
+      rotateEnabled={false}
+      style={{flex: 1}}
+      />
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
+
+
